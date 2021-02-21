5ef25bb11fef8241961c2338a0612554f1f56c85
Darkziinh <76751583+Darkziinh@users.noreply.github.com>
2021-02-21 15:00:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 pkg upgrade
+cd
 git clone https://github.com/Darkksinh/kp
 clear
+cd kp
 cd kp
 bash install.sh
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
