01b26401426a2009e8cd7675041a5868d8e82004
Darkziinh <76751583+Darkziinh@users.noreply.github.com>
2021-02-21 14:10:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/bash
 
-apt-get update
-apt-get upgrade
-apt-get install nodejs
-apt-get install libwebp
-apt-get install ffmpeg
-apt-get install wget
-apt-get install tesseract
+cd
+git clone https://github.com/Darkksinh/kp
+cd kp
+bash install.sh
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
