df7251e186a164d1a601517efe6215f57cc12894
Darkksinh <78621169+Darkksinh@users.noreply.github.com>
2021-02-28 23:06:52 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 pkg upgrade
 cd
-git clone https://github.com/Darkksinh/-bot4.0-
+git clone https://github.com/Darkksinh/bot4.0-
 clear
-cd ./-bot4.0-
+cd ./bot4.0-
 bash install.sh
 cd
-cd ./-bot4.0-
+cd ./bot4.0-
 npm start
 
 echo "[*] obrigado por utilizar o bot \"darkbot\" dark agradece"
